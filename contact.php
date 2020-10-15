98a60d31500445ef1ff17009ff8e7dce18a7a977
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-10-15 11:48:05 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Interests</title>
+	<title>Contact</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css2?family=Piazzolla:wght@300&display=swap" rel="stylesheet">
