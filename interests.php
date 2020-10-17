9b6ac3ea7ace465fc3d6e4f5c5652e1454414a7c
Mohit S Shetty <mohitsshetty986@gmail.com>
2020-10-17 10:39:10 +0530

    Update interests.php

--- interests.php
+++ interests.php
@@ -81,35 +81,7 @@
 		    <p>Aloevera cells have similar potential as the traditional batteries, at much lesser and greener cost and are safe to use and dispose as it can be easily recycled or refilled.</p>
 	    </article>
 
-	    <article>
-
-	  		<h2>Tesla Gigafactory</h2>
-
-	  		<img src="facts/dhawan-1.jpg" style="width:100%">
-	  		<br>
-
-	  		<p1>References:- <a href="https://skyroot.in/">skyroot.in</a>, <a href="https://yourstory.com/2020/09/hyderabad-spacetech-startup-skyroot-cryogenic-rocket-engine-dhawan1">yourstory.com</a>, <a href="https://manufactur3dmag.com/indias-skyroot-aerospace-test-fires-upper-stage-rocket-engine-fitted-with-3d-printed-injector/">manufactur3dmag.com</a>, <a href="https://www.3dprintingmedia.network/skyroot-aerospace-shows-off-100-3d-printed-dhawan-1-rocket-engine/#:~:text=On%20the%20occasion%20of%20the,also%20100%25%20Made%20in%20India.">3dprintingmedia.network</a></p1>
-
-	  		<p>Tesla</p>
-
-	    	<p>Skyroot is building a series of Rockets named Vikram, after Dr. Vikram Sarabhai. Vikram is built on a common architecture and covers a wide range of payloads, they offer the most affordable and on demand ride to space. It's 100% Made in India.</p>
-
-	    	<p>Compared to traditional manufacturing it reduced overall mass by 50 %, reduced total number of components and lead time by 80%. The engine is capable of multiple restarts enabling them to insert various satellites into multiple orbits in a single mission.</p>
-
-	    	<p>This cyyogenic engine is expected to be used in Vikram-II rocket for propulsion.</p>
-
-	    	<br>
-	    	<h2>Battery made of nature's extracts by Aloe E-Cell, an Indian Startup</h2>
-	  		<p1>References:- <a href="https://www.aloeecell.com/">aloeecell.com</a>, <a href="https://www.f6s.com/aloeecell">f6s.com/</a>, <a href="https://timesofindia.indiatimes.com/city/lucknow/aloe-magic-herbal-panacea-can-now-power-green-batteries/articleshow/71390317.cms">The Times Of India</a></p1>
-
-		    <p>Out of total 2 Million Metric tonnes of E-waste generated annually it contains 11.4 M batteries accounting to a share of 82% of total waste resulting in landfills causing air, water & soil pollution. 97% of the material used in traditional batteries are imported levying a high pressure of >148 Billion Dollars on Indian economy.</p>
-
-		    <img src="facts/aloeecell.jpg" style="width:60%">
-
-		    <p>Aloe E-cell have created world's first 100% Eco friendly and non hazardous 1.5V batteries using Aloevera, at 10% cheaper cost and 1.5 times durability.</p>
-
-		    <p>Aloevera cells have similar potential as the traditional batteries, at much lesser and greener cost and are safe to use and dispose as it can be easily recycled or refilled.</p>
-	    </article>
+	    
 
     <br>
     <hr width="80%;">
